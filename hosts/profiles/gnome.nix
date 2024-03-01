6c35e2906790f95195c2f676b0666427ed16dbfa
ivktac <git@tkachuk.email>
2024-03-02 00:33:45 +0200

    chore: remove unsued gnome packages

--- hosts/profiles/gnome.nix
+++ hosts/profiles/gnome.nix
@@ -1,7 +1,19 @@
-{
+{pkgs, ...}: {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-tour
+    ])
+    ++ (with pkgs.gnome; [
+      epiphany
+      gnome-music
+      totem
+      yelp
+      simple-scan
+    ]);
 }
