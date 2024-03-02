12a945f741ebcef2006fa048586f56387ba08003
ivktac <git@tkachuk.email>
2024-03-02 02:54:12 +0200

    feat: initial work profiles

--- home/default.nix
+++ home/default.nix
@@ -1,26 +1,31 @@
-{ inputs, lib, ... }:
-let
+{
+  inputs,
+  lib,
+  ...
+}: let
   sharedModules = [
     ./modules/programs/git.nix
     ./modules/programs/gpg.nix
     ./modules/programs/ssh.nix
     ./modules/programs/utils.nix
     ./modules/programs/sh.nix
-     inputs.nixvim.homeManagerModules.nixvim
+    inputs.nixvim.homeManagerModules.nixvim
     ./modules/programs/nvim.nix
     ./modules/programs/starship.nix
   ];
 
   homeImports = {
-    ivktac = [
-      ./home.nix
-      ./profiles/gnome.nix
-    ]
-    ++ lib.concatLists [sharedModules];
+    ivktac =
+      [
+        ./home.nix
+        ./profiles/gnome.nix
+        ./profiles/work.nix
+      ]
+      ++ lib.concatLists [sharedModules];
   };
 
   inherit (inputs.home-manager.lib) homeManagerConfiguration;
-  
+
   pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
 in {
   _module.args = {inherit homeImports;};
