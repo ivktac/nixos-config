985c52c45a6f4539c4f60b6d271510703bd27604
ivktac <git@tkachuk.email>
2024-03-02 02:01:22 +0200

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -1,34 +1,34 @@
 {
-	description = "My NixOS Configuration!";
+  description = "My NixOS Configuration!";
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} {imports = [./flake];};
 
-	inputs = {
-   	nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/nur";
     flake-utils.url = "github:numtide/flake-utils";
 
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		
-		nh = {
-		  url = "github:viperML/nh";
-		  inputs.nixpkgs.follows = "nixpkgs";
-		};
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nh = {
+      url = "github:viperML/nh";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     nix-index-db = {
       url = "github:Mic92/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-		
-		nixvim = {
-		  url = "github:nix-community/nixvim";
-		  inputs.nixpkgs.follows = "nixpkgs";
-		};
 
-  	flake-parts = {
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
@@ -38,5 +38,5 @@
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-	};
+  };
 }
